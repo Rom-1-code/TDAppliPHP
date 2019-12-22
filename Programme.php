9cee427ea333a413721e67575d77f96a34919859
Mathis <clermont.mailpro@gmail.com>
2019-12-22 18:25:38 +0100

    debut du dev de la premiere méthode

---
+++ Programme.php
@@ -0,0 +1,42 @@
+<?php
+
+class Programme
+{
+
+    private _typeprog;
+    
+
+
+
+    public function syncronisewithbase($this->_typeprog)
+    {
+        $Base = new PDO('mysql:host=localhost; dbname=base_sportive; charset=utf8','root','');
+        
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+?>
