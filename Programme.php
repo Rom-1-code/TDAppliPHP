46c205be66ee10b5580e506d21eadbc301bb8829
Mathis <clermont.mailpro@gmail.com>
2019-12-23 12:47:57 +0100

    ajout des requêtes SQL à la classe programme (à vérifier)

--- Programme.php
+++ Programme.php
@@ -3,14 +3,42 @@
 class Programme
 {
 
-    private _typeprog;
-    
+    private _idprog;
+    private _adressepageprog; //numero fourni par la base qui donnera accès à la page php contenant le programme sportif
 
 
 
-    public function syncronisewithbase($this->_typeprog)
+    public function syncronisewithbase($this->_idprog)
     {
-        $Base = new PDO('mysql:host=localhost; dbname=base_sportive; charset=utf8','root','');
+        try
+        {
+            $Base = new PDO('mysql:host=localhost; dbname=base_sportive; charset=utf8','root','');
+        }
+        catch($erreur error)
+        {
+            echo "pas possible de se connecter à la base";
+        }
+
+        try
+        {
+            $Base->query("INSERT into user (id_programme) VALUES ('$this->_idprog')");
+        }
+        catch($erreur error)
+        {
+            echo "pas possible d'insérer cette valeur";
+        }
+
+        try
+        {
+            $donneesbrutes=$Base->query("SELECT adressepageprogramme from programme,user where programme.id_programme=user.id_programme");
+        }
+        catch($erreur error)
+        {
+            echo "pas possible d'insérer cette valeur";
+        }
+
+
+        
         
     }
 
