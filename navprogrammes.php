87206f4db2e4966af1527389ea01eb801b4c8cf6
FLEMAL <55393709+Rom-1-code@users.noreply.github.com>
2020-01-06 12:06:24 +0100

    creation des div dans navprogrammes +css

--- navprogrammes.php
+++ navprogrammes.php
@@ -1,6 +1,7 @@
-<?php require "Users.php";?>
+<?php require "Users.php"; ?>
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -8,7 +9,59 @@
     <link rel="stylesheet" href="css/style.css">
     <title>Navigation programme</title>
 </head>
+
 <body>
-    
+    <!-- HEADER -->
+    <div class="page">
+        <header>
+            <div class="container">
+                <nav class="menu-bar">
+
+                    <!--TITRE-->
+
+                    <div class="logo">
+                        <ul class="titre">
+                            <li class="name">
+                                <a>
+                                    <h1>Fitness<span class="tld">.php</span></h1>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <!--MENU DROIT-->
+
+                    <div class="menu">
+                        <ul class="right">
+                            <li><b><a href="#">Decouvrir</a></li></b>
+                            <li><b><a href="conseil.php">Conseil</a></li></b>
+                            <li><b><a href="apropos.php">A propos</a></li></b>
+                            <li><b><a href="identification.php">S'identifier</a></li></b>
+                        </ul>
+                    </div>
+                    <div class="clearfix"></div>
+                </nav>
+        </header>
+    </div>
+    </div>
+    <!-- FIN HEADER -->
+    <div class="titre3">
+        <h1>Presentation des programmes</h1>
+    </div>
+
+    <div class="tonic">
+        <h2>Programme Tonic</h2>
+    </div>
+    <div class="intensif">
+        <h2>Programme intensif</h2>
+    </div>
+    <div class="forme">
+        <h2>Programme forme</h2>
+    </div>
+
+
+
+
 </body>
+
 </html>
